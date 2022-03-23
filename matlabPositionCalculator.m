2ca76240845e6d4eb7673e171cbe00c421cfcf1d
ViktorVektor <41247872+ViktorVektor@users.noreply.github.com>
2022-03-23 04:35:05 -0700

    Matlab script
    Input your pulley positions, then input your XYZ. Will stay in the XYZ loop, then spits out uvw ratios. Use these ratios to calculate cable lengths. Remember that the distance between each pulley is a ratio of 1

---
+++ matlabPositionCalculator.m
@@ -0,0 +1,47 @@
+prompt = "Enter Ax: ";
+Ax = input(prompt)
+prompt = "Enter Ay: ";
+Ay = input(prompt)
+prompt = "Enter Az: ";
+Az = input(prompt)
+
+prompt = "Enter Bx: ";
+Bx = input(prompt)
+prompt = "Enter By: ";
+By = input(prompt)
+prompt = "Enter Bz: ";
+Bz = input(prompt)
+
+prompt = "Enter Cx: ";
+Cx = input(prompt)
+prompt = "Enter Cy: ";
+Cy = input(prompt)
+prompt = "Enter Cz: ";
+Cz = input(prompt)
+
+S = sqrt(power((Ax*(By-Cy)+Bx*(Cy-Ay)+Cx*(Ay-By)), 2) ...
+    +power((Ay*(Bz-Cz)+By*(Cz-Az)+Cy*(Az-Bz)), 2) ...
+    +power((Az*(Bx-Cx)+Bz*(Cx-Ax)+Cz*(Ax-Bx)), 2))/2
+
+while 1 == 1
+    prompt = "Enter X: ";
+    x = input(prompt)
+    prompt = "Enter Y: ";
+    y = input(prompt)
+    prompt = "Enter Z: ";
+    z = input(prompt)
+
+    u = sqrt(power((x*(By-Cy)+Bx*(Cy-y)+Cx*(y-By)), 2)+ ...
+        power((y*(Bz-Cz)+By*(Cz-z)+Cy*(z-Bz)), 2)+ ...
+        power((z*(Bx-Cx)+Bz*(Cx-x)+Cz*(x-Bx)), 2)) / (2*S)
+
+    v = sqrt(power((Ax*(y-Cy)+x*(Cy-Ay)+Cx*(Ay-y)), 2) ...
+        +power((Ay*(z-Cz)+y*(Cz-Az)+Cy*(Az-z)), 2) ...
+        +power((Az*(x-Cx)+z*(Cx-Ax)+Cz*(Ax-x)), 2)) / (2*S)
+
+    w = sqrt(power((Ax*(By-y)+Bx*(y-Ay)+x*(Ay-By)), 2) ...
+        +power((Ay*(Bz-z)+By*(z-Az)+y*(Az-Bz)), 2) ...
+        +power((Az*(Bx-x)+Bz*(x-Ax)+z*(Ax-Bx)), 2)) / (2*S)
+    
+end
+
