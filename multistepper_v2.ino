49e86efaa8f83cbee443b9c708c7501827f47997
Evan-ED-Richardson <99714751+Evan-ED-Richardson@users.noreply.github.com>
2022-03-24 16:39:40 -0700

    Add files via upload

--- multistepper_v2.ino
+++ multistepper_v2.ino
@@ -1,15 +1,13 @@
 #include <AccelStepper.h>
+#include <MultiStepper.h>
 
-// This is how I saw steppers defined in the AccelStepper documentation, not sure if it works, NOTE step pin, direction pin
 AccelStepper stepper1(AccelStepper::DRIVER, 3, 2);
 AccelStepper stepper2(AccelStepper::DRIVER, 5, 4);
 AccelStepper stepper3(AccelStepper::DRIVER, 7, 6);
 
-long command = 0;
-long command0 = 0;
-int count = 0;
+long positions[3] = {0, 0, 0};
 
-int SPEED = 60000; // shared for all servos
+int SPEED = 5000; // shared for all servos
 int ACCEL = 1000; // also shared
 
 long destination[ ]= {0, 0, 0}; // relative to current position
@@ -20,22 +18,25 @@ char tempChars[numChars];
 
 boolean newData = false;
 
+MultiStepper steppers;
 
-void setup()
-{  
-   Serial.begin(9600);
-   Serial.setTimeout(10);
-     
-   stepper1.setMaxSpeed(SPEED);
-   stepper2.setMaxSpeed(SPEED);
-   stepper3.setMaxSpeed(SPEED);
-   stepper1.setAcceleration(ACCEL);
-   stepper2.setAcceleration(ACCEL);
-   stepper3.setAcceleration(ACCEL);
-   stepper1.setCurrentPosition(0);
-   stepper2.setCurrentPosition(0);
-   stepper3.setCurrentPosition(0);
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  delay(2000);
+
+  stepper1.setMaxSpeed(SPEED);
+  stepper2.setMaxSpeed(SPEED);
+  stepper3.setMaxSpeed(SPEED);
+  stepper1.setCurrentPosition(0);
+  stepper2.setCurrentPosition(0);
+  stepper3.setCurrentPosition(0);
+  
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
+  steppers.addStepper(stepper3);
 }
+
 void loop()
 {
   recvWithStartEndMarkers();
@@ -45,25 +46,13 @@ void loop()
             //   because strtok() used in parseData() replaces the commas with \0
         parseData();
         showParsedData();
-        stepper1.moveTo(destination[0]);
-        stepper2.moveTo(destination[1]);
-        stepper3.moveTo(destination[2]);
+        steppers.moveTo(destination);
         newData = false;
-        
     }
-  while(inMovement()){
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
+    steppers.runSpeedToPosition();
+
  }
 
- bool inMovement(){
-  if (stepper1.distanceToGo()!=0){return true;}
-  if (stepper2.distanceToGo()!=0){return true;}
-  if (stepper3.distanceToGo()!=0){return true;}
-  else {return false;}
- }
 
 void parseData() {      // split the data into its parts
     char * strtokIndx; // this is used by strtok() as an index
