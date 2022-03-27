b9d7385c6e0a40d93da08a780f22ebd48dd989d7
ViktorVektor <41247872+ViktorVektor@users.noreply.github.com>
2022-03-27 16:08:49 -0700

    Modufying offsets

--- matlabPositionCalculator.m
+++ matlabPositionCalculator.m
@@ -23,25 +23,49 @@ S = sqrt(power((Ax*(By-Cy)+Bx*(Cy-Ay)+Cx*(Ay-By)), 2) ...
     +power((Ay*(Bz-Cz)+By*(Cz-Az)+Cy*(Az-Bz)), 2) ...
     +power((Az*(Bx-Cx)+Bz*(Cx-Ax)+Cz*(Ax-Bx)), 2))/2
 
+xo = (Ax+Bx+Cx)/3;
+yo = (Ay+By+Cy)/3;
+
+% centroid cable lengths, used to subtract when homed at centroid
+%uc = 0.7702;
+%vc = 0.845;
+%wc = 0.7702;
+
+uc = 0.7793;
+vc = 0.7602;
+wc = 0.7793;
+
+steps = 40000;
+
+%offsetRatio = 0.5;
+
 while 1 == 1
     prompt = "Enter X: ";
     x = input(prompt)
     prompt = "Enter Y: ";
     y = input(prompt)
     prompt = "Enter Z: ";
+    
+
+    
+    %distFromOrigin = sqrt(power((x-xo),2) + power((y-yo),2));
+
+    %zOffset = distFromOrigin*offsetRatio;
+
     z = input(prompt)
+    %z = z-zOffset;
 
-    u = sqrt(power((x*(By-Cy)+Bx*(Cy-y)+Cx*(y-By)), 2)+ ...
+    STEPERA = (sqrt(power((x*(By-Cy)+Bx*(Cy-y)+Cx*(y-By)), 2)+ ...
         power((y*(Bz-Cz)+By*(Cz-z)+Cy*(z-Bz)), 2)+ ...
-        power((z*(Bx-Cx)+Bz*(Cx-x)+Cz*(x-Bx)), 2)) / (2*S)
+        power((z*(Bx-Cx)+Bz*(Cx-x)+Cz*(x-Bx)), 2)) / (2*S) - uc)*steps
 
-    v = sqrt(power((Ax*(y-Cy)+x*(Cy-Ay)+Cx*(Ay-y)), 2) ...
+    STEPPERB = (sqrt(power((Ax*(y-Cy)+x*(Cy-Ay)+Cx*(Ay-y)), 2) ...
         +power((Ay*(z-Cz)+y*(Cz-Az)+Cy*(Az-z)), 2) ...
-        +power((Az*(x-Cx)+z*(Cx-Ax)+Cz*(Ax-x)), 2)) / (2*S)
+        +power((Az*(x-Cx)+z*(Cx-Ax)+Cz*(Ax-x)), 2)) / (2*S) - vc)*steps
 
-    w = sqrt(power((Ax*(By-y)+Bx*(y-Ay)+x*(Ay-By)), 2) ...
+    STEPPERC = (sqrt(power((Ax*(By-y)+Bx*(y-Ay)+x*(Ay-By)), 2) ...
         +power((Ay*(Bz-z)+By*(z-Az)+y*(Az-Bz)), 2) ...
-        +power((Az*(Bx-x)+Bz*(x-Ax)+z*(Ax-Bx)), 2)) / (2*S)
+        +power((Az*(Bx-x)+Bz*(x-Ax)+z*(Ax-Bx)), 2)) / (2*S) - wc)*steps
     
 end
 
